cc2710d14d4e652257304a07f834491b58ab10ca
akshataambure123 <akshataambure123@gmail.com>
2026-09-21 17:12:03 +0000

    class and object added

--- C++Learning.cpp
+++ C++Learning.cpp
@@ -1,45 +1,24 @@
+/*Akshata Dinesh Ambure
+Computer Science Engineering
+SY-C
+*/
 #include<iostream>
 using namespace std;
 
 int main()
 {
     int choice;
-    cout<<"\t\t\t\t****C++****\n";
+    //----------------------------Theory-----------------------------------
+    /*cout<<"\t\t\t\t****C++****\n";
     cout<<"\n   Introduction to C++ Programming Language";
-    cout<<"\n 1.1 What is C++? C++ is a general-purpose, high-level programming language used to develop software applications, operating systems, games, and many other types of computer programs.It is an extension of the C programming language that introduces additional features, especially Object-Oriented Programming (OOP).
+    cout<<"\n 1.1 What is C++? C++ is a general-purpose, high-level programming language used to develop software applications, operating systems, games, and many other types of computer programs.It is an extension of the C programming language that introduces additional features, especially Object-Oriented Programming (OOP). C++ supports both procedural programming and object-oriented programming, making it a flexible and powerful programming language. It allows programmers to write programs using functions, classes, objects, and other programming concepts.";
+    cout<<"\nDefinition"<<"C++ is a general-purpose, object-oriented programming language developed as an extension of the C language. It supports procedural, object-oriented, and generic programming.";
 
-C++ supports both procedural programming and object-oriented programming, making it a flexible and powerful programming language.
+    cout<<"1.2 History of C++"<<"\n"<<"C++ was developed by Bjarne Stroustrup at Bell Laboratories.In 1979, Bjarne Stroustrup started developing a language called C with Classes. It was designed to combine the efficiency of C with the features of object-oriented programming.In 1983, the language was renamed C++. The name C++ comes from the increment operator ++ in C, indicating an improvement over C.C++ became an internationally standardized programming language in 1998 with the C++98 standard. Modern versions of C++ provide additional features that make programming safer, more efficient, and easier to maintain.";
+    cout<<"1.3 Why is C++ Important?"<<"\n"<<"C++ is important because it provides:"<<"\n"<<"High performance: It can execute programs efficiently and is suitable for performance-critical applications.Object-oriented programming: It allows programmers to organize programs using classes and objects.Code reusability: Features such as inheritance allow programmers to reuse existing code. Data security: Encapsulation and access specifiers help control access to data.Portability: C++ programs can be compiled for different platforms, although platform-specific code may require modifications.Memory management: It provides facilities for managing memory directly and indirectly.Flexibility: It supports procedural, object-oriented, and generic programming.";*/
+    //-------------------------end of theory------------------------------
 
-It allows programmers to write programs using functions, classes, objects, and other programming concepts.
-
-Definition
-
-C++ is a general-purpose, object-oriented programming language developed as an extension of the C language. It supports procedural, object-oriented, and generic programming.
-
-1.2 History of C++
-
-C++ was developed by Bjarne Stroustrup at Bell Laboratories.
-
-Important points in the history of C++:
-
-In 1979, Bjarne Stroustrup started developing a language called "C with Classes."
-It was designed to combine the efficiency of C with the features of object-oriented programming.
-In 1983, the language was renamed C++.
-The name C++ comes from the increment operator ++ in C, indicating an improvement over C.
-C++ became an internationally standardized programming language in 1998 with the C++98 standard.
-Modern versions of C++ provide additional features that make programming safer, more efficient, and easier to maintain.
-1.3 Why is C++ Important?
-
-C++ is important because it provides:
-
-High performance: It can execute programs efficiently and is suitable for performance-critical applications.
-Object-oriented programming: It allows programmers to organize programs using classes and objects.
-Code reusability: Features such as inheritance allow programmers to reuse existing code.
-Data security: Encapsulation and access specifiers help control access to data.
-Portability: C++ programs can be compiled for different platforms, although platform-specific code may require modifications.
-Memory management: It provides facilities for managing memory directly and indirectly.
-Flexibility: It supports procedural, object-oriented, and generic programming.
-1.4 Features of C++
+/*1.4 Features of C++
 1. Object-Oriented Programming
 
 C++ supports object-oriented programming, which organizes programs around classes and objects.
@@ -109,8 +88,89 @@ try
 throw
 catch
 
-These facilities help programmers handle exceptional situations.<<"\n";
+These facilities help programmers handle exceptional situations.*/
 
-cout<<"10. Generic Programming<<"\n"<<"C++ supports templates, which allow programmers to write reusable functions and classes that work with different data types.";
+    cout<<"10. Generic Programming"<<"\n"<<"C++ supports templates, which allow programmers to write reusable functions and classes that work with different data types.";
+  
+//--------------------------------------------real code----------------------------------
+    class Student
+    {
+        public:
+        int rollno;
+        string name;
+
+        void display()
+        {
+            cout << "\n--- Student Details ---" << endl;
+            cout << "Roll Number: " << rollno << endl;
+            cout << "Name: " << name << endl;
+        }
+    };
+
+//------------------------------------------Switch--------------------------------------------
+    cout << "===== C++ OOP CONCEPTS =====" << endl;
+    cout << "1. Class and Object\n2.Costructor" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch(choice)
+    {
+        case 1:
+        {
+            Student s1;
+            cout<<"\n Syntax of class-"<<"\n class ClassName"<<"\n"<<"{"<<"\n\t"<<"access_specifier:"<<"\n\t"<<"// Data members (variables)"<<"\n\t"<<"// Member functions (functions)"<<"\n"<<"};"<<"\n";
+            cout<<"\nSyntax of Object creation-\n"<<"ClassName object;"<<"\n";
+        //----------------------------------------code to display--------------
+        cout << R"(
+        let's see code of class
+         -------Program of class------
+            //header files
+            #include <iostream>
+            using namespace std;
+          
+             class Student   //class 
+             { 
+                public: 
+                int rollno; 
+                string name; 
+                    
+                void display() 
+                { 
+                    cout << "\n--- Student Details ---" << endl; 
+                    cout << "Roll Number: " << rollno << endl; 
+                     cout << "Name: " << name << endl; 
+                 } 
+             };
+
+            int main()
+             {
+                Student s;//object creation
+                cout << "\nEnter Roll Number: ";
+                cin >> s1.rollno;
+                cout << "Enter Name: ";
+                cin >> s1.name;
+
+                return 0;
+             }
+                )";
+            //----------------end of program------------------------
+
+            cout<<"\nOutput\n";
+
+            cout << "\nEnter Roll Number: ";
+            cin >> s1.rollno;
+            cout << "Enter Name: ";
+            cin >> s1.name;
+
+            s1.display();
+
+            break;
+         }
+
+        default:
+            cout << "Invalid Choice!";
+    }
+
+//---------------------------------------------------------------------------------------------
     return 0;
 }
